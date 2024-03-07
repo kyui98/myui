7008ea130e16db493c05a8c4f8ea2eed3a38da07
yui <159100068+kyui98@users.noreply.github.com>
2024-03-07 22:49:56 +0900

    select.php
    新規追加

--- php_db/select.php
+++ php_db/select.php
@@ -1,66 +1,35 @@
-<?php
-$dsh = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
-$user = 'rooy';
-$password = 'root';
-
-try {
-   $pdo = new PDO($dsh, $user, $password);
-
-   //userテーブルからidカラムとnameカラムのデータを取得するためのSQL文を変数$sqlに代入する
-   $sql = 'SELECT id, name FROM users';
-
-   //SQL文を実行する
-   $stmt = $pdo->query($sql);
-
-   //SQL文の実行結果を配列で取得する
-   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
- } catch (PDOException $e) {
-   exit($e->getMessage());
-}
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>PHP+DB</title>
-  </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP+DB</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
-  <body>
+<body>
     <p>
-
-      <?php
-      $dsh = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
-      $user = 'root';
-      $password = 'root';
-
-      try {
-        $pdo = new PDO($dsh, $user, $password);
-
-        //usersテーブルからidカラムとnameカラムのデータを取得するためのSQL文を変数$splに代入する
-        $sql = 'SELECT id, name FROM users';
-
-        //$pdo->query($sql);
-        //SQL文を実行する
-        $stmt = $pdo->query($sql);
-
-        //SQL文の実行結果を配列(results)で取得する
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        //配列の中身を出力する
-        print_r($results);
-      } catch (PDOException $e) {
-        exit($e->getMessage());
-      }
-      ?>
-      <?php
-      //配列の中身を出力する
-      print_r($results);
-      ?>
+      
+       <?php
+       //配列の中身を出力
+       print_r($results);
+       ?>
     </p>
+    <table>
+          <tr>
+             <th>ID</th>
+             <th>氏名</th>
+          </tr>
+          <?php
+          //配列の中身を順番に取り出し表彰式で出力する
+          foreach ($results as $result) {
+               echo "<tr><td>{$result['id']}</td><td>{$result['name']}</td></tr>";
+
+          }
+          ?>
+    </table>
   </body>
-  
 </html>
 
+          
